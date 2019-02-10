46dddd874163a877f554364f1812cfe4254e0a05
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2019-02-10 21:45:54 +0100

    Added Spring Boot Application

---
+++ src/main/java/org/ale/gowiththeflow/GowiththeflowApplication.java
@@ -0,0 +1,13 @@
+package org.ale.gowiththeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowiththeflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GowiththeflowApplication.class, args);
+    }
+
+}
